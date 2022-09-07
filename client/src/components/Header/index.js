76fa3ffc07322686f7277602336ec233939f449e
chathurperera <chathuraperera007@gmail.com>
2022-09-07 20:55:55 +0530

    navbar added

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import classes from "./Header.module.scss";
 import logo from "../../assets/images/BlackLogo.svg";
+import CartIcon from "../../assets/images/CartIcon.svg";
+import SearchIcon from "../../assets/images/SearchIcon.svg";
+import NavMenuButton from "../../assets/images/NavMenuButton.svg";
+
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className={classes.header}>
@@ -8,8 +13,19 @@ const Header = () => {
         <div className={classes.logo}>
           <img src={logo} alt="logo" />
         </div>
-        <nav></nav>
-        <div></div>
+        <nav>
+          <ul>
+            <li>ALL PRODUCTS</li>
+            <li>SOLUTIONS</li>
+            <li>ABOUT</li>
+            <li>SUPPORT</li>
+          </ul>
+        </nav>
+        <div className={classes.navIcons}>
+          <img src={SearchIcon} alt="Search Icon" />
+          <img src={CartIcon} alt="cart icon" />
+          <img src={NavMenuButton} className={classes.hamburgerIcon} alt="hambuger icon" />
+        </div>
       </div>
     </header>
   );
