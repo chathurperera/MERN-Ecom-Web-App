cdebea0f24d652ad65356f8282bdc5d8a63986ab
chathurperera <chathuraperera007@gmail.com>
2022-09-09 00:05:33 +0530

    megadropdown in progress

--- client/src/components/MegaDropDown/index.js
+++ client/src/components/MegaDropDown/index.js
@@ -11,7 +11,116 @@ const MegaDropDown = () => {
           <Link to="/">
             <div className={classes.categoryHolder}>
               <div className={classes.categoryImageHolder}>
-                <img src={phoneIcon} alt="phone icon" />
+                <svg
+                  width="21"
+                  height="30"
+                  viewBox="0 0 21 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.2857 1H3.71429C2.21523 1 1 2.21523 1 3.71429V25.4286C1 26.9276 2.21523 28.1429 3.71429 28.1429H17.2857C18.7848 28.1429 20 26.9276 20 25.4286V3.71429C20 2.21523 18.7848 1 17.2857 1Z"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M10.5 22.7144H10.5136"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+          </Link>
+          <Link to="/">
+            <div className={classes.categoryHolder}>
+              <div className={classes.categoryImageHolder}>
+                <svg
+                  width="21"
+                  height="30"
+                  viewBox="0 0 21 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.2857 1H3.71429C2.21523 1 1 2.21523 1 3.71429V25.4286C1 26.9276 2.21523 28.1429 3.71429 28.1429H17.2857C18.7848 28.1429 20 26.9276 20 25.4286V3.71429C20 2.21523 18.7848 1 17.2857 1Z"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M10.5 22.7144H10.5136"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+          </Link>
+          <Link to="/">
+            <div className={classes.categoryHolder}>
+              <div className={classes.categoryImageHolder}>
+                <svg
+                  width="21"
+                  height="30"
+                  viewBox="0 0 21 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.2857 1H3.71429C2.21523 1 1 2.21523 1 3.71429V25.4286C1 26.9276 2.21523 28.1429 3.71429 28.1429H17.2857C18.7848 28.1429 20 26.9276 20 25.4286V3.71429C20 2.21523 18.7848 1 17.2857 1Z"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M10.5 22.7144H10.5136"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className={classes.categoryContentHolder}>
+                <div className={classes.title}>Phones</div>
+                <div className={classes.title}>Your Day To Day Life</div>
+              </div>
+            </div>
+          </Link>
+          <Link to="/">
+            <div className={classes.categoryHolder}>
+              <div className={classes.categoryImageHolder}>
+                <svg
+                  width="21"
+                  height="30"
+                  viewBox="0 0 21 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.2857 1H3.71429C2.21523 1 1 2.21523 1 3.71429V25.4286C1 26.9276 2.21523 28.1429 3.71429 28.1429H17.2857C18.7848 28.1429 20 26.9276 20 25.4286V3.71429C20 2.21523 18.7848 1 17.2857 1Z"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M10.5 22.7144H10.5136"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
               </div>
             </div>
           </Link>
