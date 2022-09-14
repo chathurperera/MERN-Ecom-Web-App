8e460c4c3b05dbc918d66827e50abdb3e388fe8a
chathurperera <chathuraperera007@gmail.com>
2022-09-14 12:59:14 +0530

    slider in progress

--- client/src/components/HeroSlider/index.js
+++ client/src/components/HeroSlider/index.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
 import "swiper/css";
-import classes from './HeroSlider.module.scss'
+import "swiper/css/pagination";
+import classes from "./HeroSlider.module.scss";
 
 const HeroSlider = () => {
   return (
     <div className={classes.sliderWrapper}>
       <Swiper
+        pagination={true}
+        modules={[Pagination]}
         spaceBetween={50}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>
+          <div className={`${classes.slideHolder} ${classes.slideOne}`}>
+            <div className={classes.slideContent}>
+              <h1>Your Pro setup just got better</h1>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta,
+                quidem.
+              </p>
+            </div>
+            <div className={classes.slideImage}></div>
+          </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
