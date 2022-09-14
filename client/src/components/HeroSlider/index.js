bd383f168fa1b50938f4bf590219511b6c5c3021
chathurperera <chathuraperera007@gmail.com>
2022-09-14 14:44:39 +0530

    first slider added

--- client/src/components/HeroSlider/index.js
+++ client/src/components/HeroSlider/index.js
@@ -1,37 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
+import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
+import sliderImage1 from "../../assets/images/sliderImage1.png";
+import sliderImage2 from "../../assets/images/sliderImage2.png";
 import classes from "./HeroSlider.module.scss";
-
 const HeroSlider = () => {
   return (
     <div className={classes.sliderWrapper}>
       <Swiper
+        navigation={true}
         pagination={true}
-        modules={[Pagination]}
+        modules={[Pagination, Navigation]}
         spaceBetween={50}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>
+        <SwiperSlide className={classes.slider}>
           <div className={`${classes.slideHolder} ${classes.slideOne}`}>
             <div className={classes.slideContent}>
-              <h1>Your Pro setup just got better</h1>
+              <h1>The new phones are here take a look.</h1>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta,
-                quidem.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Scelerisque in est dui, aliquam, tempor. Faucibus morbi turpis.
               </p>
+              <Link to="/">Explore</Link>
             </div>
-            <div className={classes.slideImage}></div>
+            <div className={classes.slideImage}>
+              <img src={sliderImage1} alt="" />
+            </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
+        <SwiperSlide className={classes.slider}>
+          <div className={`${classes.slideHolder} ${classes.slideOne}`}>
+            <div className={classes.slideContent}>
+              <h1>The new phones are here take a look.</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Scelerisque in est dui, aliquam, tempor. Faucibus morbi turpis.
+              </p>
+              <Link to="/">Explore</Link>
+            </div>
+            <div className={classes.slideImage}>
+              <img src={sliderImage1} alt="" />
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,14 +1,61 @@
-import React from 'react'
-import Header from '../../components/Header'
-import HeroSlider from '../../components/HeroSlider'
-import classes from './Home.module.scss'
+import React from "react";
+import Header from "../../components/Header";
+import HeroSlider from "../../components/HeroSlider";
+import classes from "./Home.module.scss";
 const Home = () => {
   return (
     <div className={classes.title}>
-        <Header />
-        <HeroSlider />
+      <Header />
+      <HeroSlider />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+        tenetur obcaecati voluptas veniam, nemo recusandae modi magni dicta
+        impedit repellat asperiores libero commodi quos quasi aliquid
+        necessitatibus? Explicabo commodi ad nisi vel illum in est quaerat.
+        Magnam, eum. Quam voluptatem, ea rem doloribus, tempore maiores expedita
+        quos explicabo accusamus, corporis itaque. Omnis, quod similique odio
+        magnam ex, dicta ullam inventore tenetur nisi id veritatis ab saepe
+        praesentium vitae reiciendis. Cum voluptatum quam hic quia perferendis
+        similique repellendus nulla architecto, obcaecati voluptatem autem
+        reprehenderit animi blanditiis numquam veniam tempore eum expedita
+        accusantium unde aperiam officia magni beatae provident. Eaque,
+        praesentium hic veniam optio voluptates dolor officia obcaecati maiores
+        sapiente a voluptatem tenetur necessitatibus autem? Omnis soluta
+        voluptatem possimus illum asperiores itaque dolor veritatis, corrupti,
+        dolorem eveniet laudantium eligendi sint debitis deserunt ducimus vel
+        excepturi ut cupiditate vitae molestiae? Ea dolore tenetur quisquam
+        magni atque reprehenderit, quasi ut ad corrupti esse numquam odio quidem
+        magnam. Autem, sint voluptas deserunt blanditiis tempore doloribus
+        corrupti optio repellat, animi facere quas ad aperiam cumque, beatae
+        esse eum dolorum soluta perspiciatis numquam temporibus maiores! Velit
+        vel praesentium distinctio natus facilis non. Nesciunt provident totam
+        nemo labore pariatur reprehenderit ex? Quisquam, suscipit. Culpa
+        consequatur consequuntur ipsa similique pariatur earum laboriosam. Quos
+        voluptas voluptate sed nobis quod suscipit sequi enim perferendis
+        adipisci asperiores labore quaerat quo ea repellendus sunt ipsam nisi,
+        beatae obcaecati cum. Sint, enim necessitatibus obcaecati aperiam
+        voluptas unde officia soluta incidunt veritatis ipsum nemo quis
+        praesentium, quod fugit nulla. Placeat aliquam aperiam in, et dolorem
+        molestiae veniam, sit non rerum debitis quae minus harum recusandae
+        omnis facere perferendis nihil tempore velit repudiandae. Quia velit
+        accusantium sit minus sequi ipsam facere culpa consectetur dignissimos
+        soluta, praesentium distinctio a eligendi ea repellat quibusdam sed
+        quidem fuga ipsa nulla? Possimus sequi odio sint, omnis officiis
+        accusamus vitae optio voluptate pariatur perspiciatis neque! Magnam,
+        velit unde incidunt nostrum ut repudiandae iste eius neque iusto
+        laboriosam a nam, non quia inventore sed tempore! Ad, corporis.
+        Sapiente, nostrum debitis consequuntur fuga, accusantium ea doloremque
+        maxime nam sunt eveniet in atque repudiandae numquam veniam porro
+        repellat eum reprehenderit voluptatem reiciendis quos! Sapiente fuga
+        saepe, nobis vero quibusdam fugit ad blanditiis, architecto, deleniti
+        officia voluptate consectetur nihil voluptatem consequuntur dolorem quod
+        cum neque atque incidunt vitae tempore debitis perspiciatis! Voluptates
+        expedita quia sequi at, sint quos qui quo delectus similique eum
+        cupiditate possimus quidem, voluptatum dolores nulla, quas est autem
+        maxime impedit laborum.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
