a048c0e1c826929067c416c8d8dc5eeeb3a0bf99
chathurperera <chathuraperera007@gmail.com>
2022-09-14 14:55:59 +0530

    slider added

--- client/src/components/HeroSlider/index.js
+++ client/src/components/HeroSlider/index.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
-import { Navigation } from "swiper";
+import { Pagination, Navigation, EffectFade, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/effect-fade";
 import sliderImage1 from "../../assets/images/sliderImage1.png";
 import sliderImage2 from "../../assets/images/sliderImage2.png";
+import sliderImage3 from "../../assets/images/sliderImage3.png";
 import classes from "./HeroSlider.module.scss";
 const HeroSlider = () => {
   return (
     <div className={classes.sliderWrapper}>
       <Swiper
+        effect={"fade"}
         navigation={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
         pagination={true}
-        modules={[Pagination, Navigation]}
+        modules={[Pagination, Navigation, EffectFade , Autoplay]}
         spaceBetween={50}
         slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
       >
         <SwiperSlide className={classes.slider}>
           <div className={`${classes.slideHolder} ${classes.slideOne}`}>
@@ -48,6 +52,21 @@ const HeroSlider = () => {
             </div>
             <div className={classes.slideImage}>
               <img src={sliderImage2} alt="" />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={classes.slider}>
+          <div className={`${classes.slideHolder} ${classes.slideOne}`}>
+            <div className={classes.slideContent}>
+              <h1>Your Pro setup just got better.</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Scelerisque in est dui, aliquam, tempor. Faucibus morbi turpis.
+              </p>
+              <Link to="/">Explore</Link>
+            </div>
+            <div className={classes.slideImage}>
+              <img src={sliderImage3} alt="" />
             </div>
           </div>
         </SwiperSlide>
