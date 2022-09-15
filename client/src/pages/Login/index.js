45c49310d48d549ef151f735623db8f00dec0d35
chathurperera <chathuraperera007@gmail.com>
2022-09-15 22:36:13 +0530

    login page added

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -6,29 +6,31 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <main className={classes.login}>
-      <div className={classes.loginBanner}>
+      <div className={classes.loginLeft}>
         <div className={classes.content}>
           <p>iPhone 13 Pro</p>
           <h1>Oh. So. Pro.</h1>
         </div>
         <img src={sliderImage1} alt="phones" />
       </div>
-      <div className={classes.loginWrapper}>
-        <h5>Login</h5>
-        <form action="">
-          <div className={classes.inputHolder}>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="" id="" />
-          </div>
-          <div className={classes.inputHolder}>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="" id="" />
-          </div>
-          <button>Log in</button>
-        </form>
-        <p className={classes.newAccount}>
-          New to California ? <Link to="/">Create an Account</Link>{" "}
-        </p>
+      <div className={classes.loginRight}>
+        <div className={classes.loginWrapper}>
+          <h4>Login</h4>
+          <form action="">
+            <div className={classes.inputHolder}>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="" id="" />
+            </div>
+            <div className={classes.inputHolder}>
+              <label htmlFor="password">Password</label>
+              <input type="password" name="" id="" />
+            </div>
+            <button>Log in</button>
+          </form>
+          <p className={classes.newAccount}>
+            New to California ? <Link to="/">Create an Account</Link>{" "}
+          </p>
+        </div>
       </div>
     </main>
   );
