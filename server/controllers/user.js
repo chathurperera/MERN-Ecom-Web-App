8368a37f0dd635fee1f449b30ebcbc4821606b91
chathurperera <chathuraperera007@gmail.com>
2022-09-16 17:14:24 +0530

    login route setup

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,2 +1,13 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const bycrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+};
+
+const signUp = async (req, res) => {
+  const { email, password } = req.body;
+};
+
+module.exports = { login, signUp };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { login, signUp } = require("../controllers/user");
 
-router.route('/login').post()
+router.route("/login").post(login);
+
+module.exports = router;
