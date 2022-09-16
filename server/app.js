a35d40ddb4e23140c5e00b4972562280e0516cc3
chathurperera <chathuraperera007@gmail.com>
2022-09-16 16:36:26 +0530

    DB connected

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
+const connectDB = require("./db/connect");
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`server is listening on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
