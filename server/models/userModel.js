2b4259244ec4446b1aa1ba9cbc979db80671d8ca
chathurperera <chathuraperera007@gmail.com>
2022-09-16 17:07:35 +0530

    user model created

---
+++ server/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
