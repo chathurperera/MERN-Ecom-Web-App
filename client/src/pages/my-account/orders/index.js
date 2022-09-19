5fab9558c8235184d05af234b838b242add36fb1
chathurperera <chathuraperera007@gmail.com>
2022-09-19 23:33:46 +0530

    orders ui done

--- client/src/pages/my-account/orders/index.js
+++ client/src/pages/my-account/orders/index.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import classes from './orders.module.scss';
+import React from "react";
+import classes from "./orders.module.scss";
 
 const orders = () => {
   return (
     <div className={classes.orders}>
-        
+      <div className={classes.ordersHolder}>
+        <table>
+          <thead>
+            <tr>
+              <th>Order</th>
+              <th>Date</th>
+              <th>Status</th>
+              <th>Total</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>#568908</td>
+              <td>August 29, 2022</td>
+              <td>Completed</td>
+              <td>LKR 4,025.00 for 1 item</td>
+              <td>
+                <div>
+                  <button>View</button>
+                  <button>Invoice</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default orders
+export default orders;
