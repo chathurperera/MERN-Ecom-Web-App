cad7d2dc8a64f05d3f43dc9e38d2846f5d40d0ba
chathurperera <chathuraperera007@gmail.com>
2022-09-24 23:46:26 +0530

    cart item create

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./CartItem.module.scss";
-import itemImage from '../../assets/images/Watch Image-min.png';
-import deleteIcon from '../../assets/images/delete.png';
+import itemImage from "../../assets/images/Watch Image-min.png";
+import deleteIcon from "../../assets/images/delete.png";
 
 const CartItem = () => {
   return (
@@ -11,10 +11,17 @@ const CartItem = () => {
       </div>
       <div className={classes.cartItem__content}>
         <div className={classes.cartItem__name}>
-            <p className={classes.itemName}>Smart Galaxy Watch 3</p>
-            <img src={deleteIcon} alt="delete icon" />
+          <p className={classes.itemName}>Smart Galaxy Watch 3</p>
+          <img src={deleteIcon} alt="delete icon" />
         </div>
-        <div className={classes.cartItem__price}></div>
+        <div className={classes.cartItem__price}>
+          <div className={classes.itemCount}>
+            <button className={classes.decrement}>-</button>
+            <div className={classes.count}>5</div>
+            <button className={classes.increment}>+</button>
+          </div>
+          <div className={classes.price}>$ 199.00 USD</div>
+        </div>
       </div>
     </div>
   );
