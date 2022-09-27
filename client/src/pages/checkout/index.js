f3f8522c564836fbc3fcdf014cd0bd71da4db00a
chathurperera <chathuraperera007@gmail.com>
2022-09-28 00:29:09 +0530

    checkout progress bar components break

--- client/src/pages/checkout/index.js
+++ client/src/pages/checkout/index.js
@@ -1,56 +1,12 @@
 import React, { useState } from "react";
 import classes from "./checkout.module.scss";
-import check from "../../assets/images/check.png";
+
+import CheckoutProgressBar from "../../components/CheckoutProgressBar";
 const Checkout = () => {
   const [checkoutStep, setCheckoutStep] = useState(2);
   return (
     <div className={classes.checkout}>
-      <div className={classes.stepProgress}>
-        <div
-          className={
-            checkoutStep === 1 || 2 || 3
-              ? `${classes.step} ${classes.currentStep}`
-              : `${classes.step}`
-          }
-        >
-          <div className={classes.dot}>
-            {checkoutStep === 2 || checkoutStep === 3 ? (
-              <img src={check} alt="check icon" />
-            ) : (
-              "1"
-            )}
-          </div>
-          <small>Cart</small>
-        </div>
-        <div
-          className={
-            checkoutStep === 2 || checkoutStep === 3
-              ? `${classes.step} ${classes.currentStep}`
-              : `${classes.step}`
-          }
-        >
-          <div className={classes.dot}>
-            {checkoutStep === 2 || checkoutStep === 3 ? (
-              <img src={check} alt="check icon" />
-            ) : (
-              "2"
-            )}
-          </div>
-          <small>Billing & Address</small>
-        </div>
-        <div
-          className={
-            checkoutStep === 3
-              ? `${classes.step} ${classes.currentStep}`
-              : `${classes.step}`
-          }
-        >
-          <div className={classes.dot}>
-            {checkoutStep === 3 ? <img src={check} alt="check icon" /> : "3"}
-          </div>
-          <small>Cart</small>
-        </div>
-      </div>
+      <CheckoutProgressBar checkoutStep={checkoutStep} />
     </div>
   );
 };
