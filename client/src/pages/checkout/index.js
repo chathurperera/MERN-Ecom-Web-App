946316f0af18f8db2e0227cf5a6fc72415846baf
chathurperera <chathuraperera007@gmail.com>
2022-09-28 01:23:05 +0530

    checkout cart table head developed

--- client/src/pages/checkout/index.js
+++ client/src/pages/checkout/index.js
@@ -7,6 +7,29 @@ const Checkout = () => {
   return (
     <div className={classes.checkout}>
       <CheckoutProgressBar checkoutStep={checkoutStep} />
+      <div className={classes.order}>
+        <div className={classes.orderTable}>
+          <div className={classes.orderTable__card}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Total Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className={classes.orderSummary}></div>
+      </div>
     </div>
   );
 };
