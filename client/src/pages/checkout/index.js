2d108a0bc04759e8d48d4c970ee1559c47523657
chathurperera <chathuraperera007@gmail.com>
2022-09-28 00:19:35 +0530

    checkout progress bar styles done

--- client/src/pages/checkout/index.js
+++ client/src/pages/checkout/index.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./checkout.module.scss";
-
+import check from "../../assets/images/check.png";
 const Checkout = () => {
+  const [checkoutStep, setCheckoutStep] = useState(3);
   return (
     <div className={classes.checkout}>
       <div className={classes.stepProgress}>
-        <div className={classes.step}>
-          <div className={classes.dot}></div>
+        <div
+          className={
+            checkoutStep === 1 || 2 || 3
+              ? `${classes.step} ${classes.currentStep}`
+              : `${classes.step}`
+          }
+        >
+          <div className={classes.dot}>
+            {checkoutStep === 2 || checkoutStep === 3 ? (
+              <img src={check} alt="check icon" />
+            ) : (
+              "1"
+            )}
+          </div>
           <small>Cart</small>
         </div>
-        <div className={classes.step}>
-          <div className={classes.dot}></div>
+        <div
+          className={
+            checkoutStep === 2 || checkoutStep === 3
+              ? `${classes.step} ${classes.currentStep}`
+              : `${classes.step}`
+          }
+        >
+          <div className={classes.dot}>
+            {checkoutStep === 2 || checkoutStep === 3 ? (
+              <img src={check} alt="check icon" />
+            ) : (
+              "2"
+            )}
+          </div>
           <small>Billing & Address</small>
         </div>
-        <div className={classes.step}>
-          <div className={classes.dot}></div>
+        <div
+          className={
+            checkoutStep === 3
+              ? `${classes.step} ${classes.currentStep}`
+              : `${classes.step}`
+          }
+        >
+          <div className={classes.dot}>
+            {checkoutStep === 3 ? <img src={check} alt="check icon" /> : "3"}
+          </div>
           <small>Cart</small>
         </div>
       </div>
