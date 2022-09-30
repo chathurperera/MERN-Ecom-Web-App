c81b709d0b29dfeea34a5eae1d10c9d115d2c00f
chathurperera <chathuraperera007@gmail.com>
2022-09-30 21:44:24 +0530

    payment options created

--- client/src/pages/checkout/index.js
+++ client/src/pages/checkout/index.js
@@ -4,8 +4,9 @@ import CheckoutProgressBar from "components/CheckoutProgressBar";
 import OrdersTable from "components/OrdersTable";
 import OrderSummary from "components/OrderSummary";
 import SelectAddress from "components/SelectAddress";
+import PaymentOptions from "components/PaymentOptions";
 const Checkout = () => {
-  const [checkoutStep, setCheckoutStep] = useState(2);
+  const [checkoutStep, setCheckoutStep] = useState(3);
 
   return (
     <div className={classes.checkout}>
@@ -13,6 +14,7 @@ const Checkout = () => {
       <div className={classes.order}>
         {checkoutStep === 1 && <OrdersTable />}
         {checkoutStep === 2 &&  <SelectAddress setCheckoutStep={setCheckoutStep} />}
+        {checkoutStep === 3 && <PaymentOptions />}
         <OrderSummary checkoutStep={checkoutStep} />
       </div>
     </div>
