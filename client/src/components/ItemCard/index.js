021cee7111537d08286249501e6567ba6996dca2
chathurperera <chathuraperera007@gmail.com>
2022-10-03 16:16:51 +0530

    best sellers section changes

--- client/src/components/ItemCard/index.js
+++ client/src/components/ItemCard/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import classes from "./ItemCard.module.scss";
 import macBook from "../../assets/images/Mac Book 3.png";
 import { Link } from "react-router-dom";
-
+import SPORT5SHORTS from 'assets/images/home/SPORT5SHORTS.png';
 const ItemCard = () => {
   return (
     <div className={classes.itemCard}>
       <Link to="">
         <div className={classes.cardHolder}>
           <div className={classes.imageHolder}>
-            <img src={macBook} className={classes.itemImage} alt="product item" />
+            <img src={SPORT5SHORTS} className={classes.itemImage} alt="product item" />
           </div>
           <div className={classes.contentHolder}>
-            <h3 className={classes.title}>MacBook Pro 13</h3>
+            <h3 className={classes.title}>SPORT 5 SHORTS</h3>
             <p className={classes.desc}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, cum
               minus. Commodi praesentium 
