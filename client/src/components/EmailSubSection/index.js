cf6705feb330a700f50e7f4502398f0d045a8c2d
chathurperera <chathuraperera007@gmail.com>
2022-10-03 16:25:29 +0530

    signup cta

--- client/src/components/EmailSubSection/index.js
+++ client/src/components/EmailSubSection/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import classes from "./EmailSubSection.module.scss";
-import ctaImage from "../../assets/images/Sign Up Image (2).png";
 const EmailSubSection = () => {
   return (
     <section className={classes.emailSubSection}>
       <div className={classes.wrapper}>
         <h2>Never miss a thing</h2>
-        <p>Be the first to know about latest tech </p>
-        <img src={ctaImage} alt="A laptop  mobile phone and a watch" />
+        <p>Be the first to know about latest trends </p>
+        {/* <img src={ctaImage} alt="A laptop  mobile phone and a watch" /> */}
         <div className={classes.inputBox}>
           <input type="text" placeholder="Your email" />
           <button>Subscribe</button>
