2f305e6546869b449dc73e25880ba241cc399d8a
chathurperera <chathuraperera007@gmail.com>
2022-10-04 15:42:59 +0530

    selected filters setup

--- client/src/components/SelectedFilters/index.js
+++ client/src/components/SelectedFilters/index.js
@@ -1,24 +1,29 @@
 import React from "react";
 import classes from "./SelectedFilters.module.scss";
 import close from "../../assets/images/cancel.png";
-const SelectedFilters = ({filters}) => {
-  const numArray = [1, 2, 3, 4, ];
+const SelectedFilters = ({ filters }) => {
+  const numArray = [1, 2, 3, 4];
   // Get the filters and create an array to display filtering options
-  return (
-    <div className={classes.selectedFilters}>
-      {numArray.map((option) => {
-        return (
-          <div className={classes.filter} key={option}>
-            <p>phones</p>
-            <div className={classes.closeIcon}>
-              <img src={close} alt="close icon" />
-            </div>
-            
-          </div>
-        );
-      })}
-    </div>
+  const filterTruthyValues = Object.keys(filters).filter(
+    (value) => !!filters[value]
   );
+  console.log("filterTruthyValues", filterTruthyValues);
+
+  const filtersList =
+    filterTruthyValues.length !== 0 &&
+    filterTruthyValues.map((option, index) => {
+      return (
+        <div className={classes.filter} key={index}>
+          <p>{option}</p>
+          <div className={classes.closeIcon}>
+            <img src={close} alt="close icon" />
+          </div>
+        </div>
+      );
+    });
+
+
+  return <div className={classes.selectedFilters}>{filtersList}</div>;
 };
 
 export default SelectedFilters;
