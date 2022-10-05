292af93b0a3883f780eb973f52df4e02746898e2
chathurperera <chathuraperera007@gmail.com>
2022-10-05 23:46:12 +0530

    routing setup

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Routing from './routing';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>
 );
 
