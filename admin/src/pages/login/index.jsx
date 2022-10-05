7043840a73ec19146d99820bd99987a7553d44cf
chathurperera <chathuraperera007@gmail.com>
2022-10-05 23:23:12 +0530

    admin login created

--- admin/src/pages/login/index.jsx
+++ admin/src/pages/login/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import classes from './login.module.scss';
+import React from "react";
+import classes from "./login.module.scss";
 
 const Login = () => {
   return (
-    <div>
-        login
+    <div className={classes.login}>
+      <div className={classes.loginHolder}>
+        <h3>Welcome back</h3>
+        <div className={classes.inputHolder}>
+          <label>Email</label>
+          <input type="text" placeholder="enter your email" />
+        </div>
+        <div className={classes.inputHolder}>
+          <label>Password</label>
+          <input type="password" placeholder="enter your password" />
+        </div>
+        <button>Login</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
