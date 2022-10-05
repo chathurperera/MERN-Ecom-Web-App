2c95e6b35450f95dafc40947ba54201b574dcb72
chathurperera <chathuraperera007@gmail.com>
2022-10-05 23:54:58 +0530

    layouts structure added

---
+++ admin/src/layouts/panelLayout/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PanelLayout = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default PanelLayout
--- admin/src/routing.js
+++ admin/src/routing.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import PanelLayout from "./layouts/panelLayout";
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<Login />} />
+        <Route path="/" element={<PanelLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
