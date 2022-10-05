1ecdd62b54a037644ff808ad8e304ec24b2f588e
chathurperera <chathuraperera007@gmail.com>
2022-10-06 02:31:37 +0530

    sidebar ui added

---
+++ admin/src/components/Sidebar/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Sidebar.module.scss";
+import { Link } from "react-router-dom";
+import dashboardicon from '../../assets/images/dashboard.svg'
+import shoppingBag from '../../assets/images/shoppingBag.svg'
+import addProducts from '../../assets/images/addProducts.svg'
+const Sidebar = () => {
+  return (
+    <div className={classes.sidebar}>
+      <div className={classes.sidebar__option}>
+          <Link to=""><img src={dashboardicon} alt="" /> Dashboard</Link>
+      </div>
+      <div className={classes.sidebar__option}>
+          <Link to=""><img src={shoppingBag} alt="" /> Products</Link>
+      </div>
+      <div className={classes.sidebar__option}>
+          <Link to=""><img src={addProducts} alt="" /> Add Product</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- admin/src/layouts/panelLayout/index.js
+++ admin/src/layouts/panelLayout/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import Sidebar from '../../components/Sidebar';
+import classes from './panelLayout.module.scss';
 
 const PanelLayout = () => {
   return (
-    <div>
+    <div className={classes.layout}>
+        <Sidebar />
         <Outlet />
     </div>
   )
