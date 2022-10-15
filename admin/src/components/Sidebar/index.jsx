5f4b8ec5cbaf1eac73e5314ef72447a147d0d97b
chathurperera <chathuraperera007@gmail.com>
2022-10-15 23:26:07 +0530

    admin sidebar

--- admin/src/components/Sidebar/index.jsx
+++ admin/src/components/Sidebar/index.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import classes from "./Sidebar.module.scss";
 import { Link } from "react-router-dom";
-import dashboardicon from "../../assets/images/dashboard.svg";
+import dashboardIcon from "../../assets/images/sidebar/dashboard.png";
+import ordersIcon from "../../assets/images/sidebar/shopping-cart.png";
+import groupIcon from "../../assets/images/sidebar/group.png";
 import shoppingBag from "../../assets/images/shoppingBag.svg";
 import addProducts from "../../assets/images/addProducts.svg";
 const Sidebar = () => {
   return (
     <div className={classes.sidebar}>
-      <div className={classes.head}>Welcome Jon doe</div>
+      <div className={classes.head}>
+        <div className={classes.avatar}>
+          <p>J</p>
+        </div>
+        <div className={classes.adminName}>
+          <p>Welcome back</p>
+          <p>Jon doe</p>
+        </div>
+      </div>
       <div className={classes.sidebar__option}>
         <Link to="/">
-          <img src={dashboardicon} alt="dashboard icon" /> Dashboard
+          <img src={dashboardIcon} alt="dashboard icon" /> Dashboard
         </Link>
       </div>
       <div className={classes.sidebar__option}>
@@ -21,6 +31,16 @@ const Sidebar = () => {
       <div className={classes.sidebar__option}>
         <Link to="/create-product">
           <img src={addProducts} alt="add icon" /> Add Product
+        </Link>
+      </div>
+      <div className={classes.sidebar__option}>
+        <Link to="/all-order">
+          <img src={ordersIcon} alt="all orders icon" /> Orders
+        </Link>
+      </div>
+      <div className={classes.sidebar__option}>
+        <Link to="/all-users"> 
+          <img src={groupIcon} alt="all users icon" /> Users
         </Link>
       </div>
     </div>
