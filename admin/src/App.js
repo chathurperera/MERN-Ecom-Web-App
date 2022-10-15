74d0ce83bda5dc3108282c3900568812938c3fb9
chathurperera <chathuraperera007@gmail.com>
2022-10-15 19:30:01 +0530

    Rout

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import Login from './pages/login'
-import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import PanelLayout from "./layouts/panelLayout";
+import Products from "./pages/products";
+import EditProduct from "pages/edit-product";
+import CreateProduct from "pages/create-product";
+import "../src/App.scss";
 
 const App = () => {
   return (
-    <main>
-      <Login />
-    </main>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/" element={<PanelLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="create-product" element={<CreateProduct />} />
+          <Route path="products/edit/:id" element={<EditProduct />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- admin/src/index.js
+++ admin/src/index.js
@@ -4,13 +4,18 @@ import Routing from "./routing";
 import { Provider } from "react-redux";
 import { store, persistor } from "app/store";
 import { PersistGate } from "redux-persist/lib/integration/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Routing />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Routing />}></Route>
+          </Routes>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
