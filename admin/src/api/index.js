fe12fcc8dcb2864e34689d9bfdfdd3f73438cf15
chathurperera <chathuraperera007@gmail.com>
2022-10-15 18:04:22 +0530

    required packages installed

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const token = localStorage.getItem("token");
-console.log('token setup')
-console.log('token ',token)
+console.log("token setup");
+console.log("token ", token);
+
 const API = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
