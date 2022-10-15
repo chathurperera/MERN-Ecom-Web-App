c72bf8bdfafb5612b8f163829eee3aee09c9fb9f
chathurperera <chathuraperera007@gmail.com>
2022-10-15 18:29:08 +0530

    Redux persist gate setup

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Routing from './routing';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Routing from "./routing";
+import { Provider } from "react-redux";
+import { store, persistor } from "app/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Routing />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routing />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
-
