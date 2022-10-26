1cb49b2232b4febd87d7c64863c8659847bff368
chathurperera <chathuraperera007@gmail.com>
2022-10-26 16:03:17 +0530

    allowedOrigins updated

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
+  "https://mern-ecommerce-admin.vercel.app",
   "https://mern-ecom-webapp.onrender.com",
   "https://www.mern-ecom-webapp.onrender.com",
 ];
