d06c466489ba458b2467e2964b8bcc11f908863a
chathurperera <chathuraperera007@gmail.com>
2022-11-23 02:26:09 +0530

    redirects server requests to index.html

--- server/server.js
+++ server/server.js
@@ -35,11 +35,10 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   //SET STATIC FOLDER
   app.use(express.static("client/build"));
-  
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
   });
-  
 }
 
 //CONNECTING TO THE DATABASE
